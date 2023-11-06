58a7217b3f306c95b50ecaae46aa0b5db1085901
Leo Liu <leoliu8@deloitte.com>
2023-11-05 20:26:25 -0500

    updated generator script

--- generate-cli-project.sh
+++ generate-cli-project.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo Usage: $0 NEW_PROJECET_NAME NEW_BINARY_NAME
 echo Example: $0 MyCLIProject my-cli
+if [ "$#" -ne 3 ]; then
+    exit -1
+fi
 cp -r template $1
-sprl $1/ SwiftCLITemplate $1 -s
-sprl $1/ swift-cli-template $2 -s
+utility/srpl $1/ SwiftCLITemplate $1 -s
+utility/srpl $1/ swift-cli-template $2 -s
